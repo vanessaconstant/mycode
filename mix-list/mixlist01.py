a465b00595c1ea9c2b76fb1617bdf2adfdaa0221
vanessaconstant <vaconstant@gmail.com>
2022-09-28 20:44:38 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+my_list = ["192.168.0.5", 5060, "UP"]
+
+#display the first item in the IP address
+print("The first item in the list (IP): " + my_list[0] )
+
+#display the first item in the port
+print("The second item in the list (port): " + str(my_list[1]) )
+
+#display the first item in the IP state
+print("The last item in the lsit (state): " + my_list[2] ) 
