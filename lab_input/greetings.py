b70b79cf20e2c17fbd18eb94cff20689244946ba
vanessaconstant <vaconstant@gmail.com>
2022-09-28 19:48:58 +0000

    greetings challenge

---
+++ lab_input/greetings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#Ask the user for his name
+
+name_input = input("Please enter your name: ")
+
+#Ask user for day of the week
+
+day_of_week_input = input("What is the day of the week? ")
+
+
+#display greeting
+
+print(f"Hellow, {name_input}  Happy {day_of_week_input}!")
