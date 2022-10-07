29e58be1b8197d21957c5e950d6a7dcea691bd64
Vanessa Constant <vaconstant@gmail.com>
2022-10-07 02:12:14 -0400

    ran pytlint and made some updates

--- classProject/server.py
+++ classProject/server.py
@@ -1,48 +1,49 @@
 #!/usr/bin/env python3
-"""TLG Cohort | Vconstant
-    File managing routing and functionality for the website"""
+""" FoodLoop Web Application | VConstant
+    File handles all the forms for the application"""
 
-from crypt import methods
-from flask import Flask, render_template, redirect, request, session, flash
+from datetime import date
+from flask import Flask, render_template, redirect, session, flash
 import requests
-import os
 from flask_sqlalchemy import SQLAlchemy
 import forms
 from models import User, FoodItem
 from __init__ import app, db
 from flask_bcrypt import Bcrypt
-from datetime import date
 
-
+#This call initializes the object that will be hashing the passwords
 bcrypt = Bcrypt()
-
 
 @app.before_first_request
 def create_tables():
+    """This function initializes the database"""
     db.create_all()
-
 
 #ROUTES
 # Index Page route
 @app.route('/')
 def index():  
+    """This function routes to the index page"""
     return render_template('index.html')
-
-
 
 # Dashboard Page
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
+    """This function routes to the index page"""
     if not session:
         return redirect('/')
 
+    #grabbing the name to welcome the user
     login_user = User.query.get(session['users_id'])
     name = login_user.fname
 
+    #This form let's the user filter their food log by date
     form5 = forms.ChooseDateForm()
     date_input = form5.date_selected.data
-    print(date_input)
-
+    
+    #initializes date selected 
+    ##then sets date to current date if user did not select a date yet
+    #filters to the selected date once users uses the form
     date_selected = False
 
     if(date_input):
@@ -52,17 +53,21 @@ def dashboard():
         date_selected = date.today()
         print("todays date ran")
 
+    #displays date in a user friendly format
     display_date = date_selected.strftime("%A %B %d %Y")
-   
+    
+    #calls the function that filters the logs by date
     food_log = daily_food_log(date_selected)
     
     return render_template('dashboard.html', name=name, 
-    userFoodList=food_log[0], 
+    user_food_list=food_log[0], 
     totals=food_log[1], form5=form5, display_date=display_date)
 
 # Search Page
 @app.route('/searchPage')
-def searchPage():
+def search_page():
+    """This function routes to the search page"""
+
     form = forms.SearchForm()
 
     return render_template('searchPage.html', form=form)
@@ -70,19 +75,18 @@ def searchPage():
 #Search functionality calling API and giving results
 @app.route('/search', methods=['GET','POST'])
 def search():
-
+    """This function allows the user to search for foods"""
     form = forms.SearchForm()
 
     if form.validate_on_submit():
         food = form.food.data
-        print(food)
         form.food.data = ''
 
     #calling api function
-    result = apiCall(food)
+    result = api_call(food)
 
     #crate empty list to capture all 10 results
-    resultList = []
+    result_list = []
    
     #print(result)
     for i in range(0,10):
@@ -97,17 +101,14 @@ def search():
 
         }
         
-        resultList.append(data)
-     
-    
-    results = resultList
+        result_list.append(data)
        
-    return render_template('searchPage.html', results=results, form=form, food= food)
+    return render_template('searchPage.html', result_list=result_list, form=form, food= food)
 
 #Registration page
 @app.route('/register', methods=['GET','POST'])
 def register():
-    
+    """This function allows the user to register for an account"""
     form2 = forms.UserForm()
 
     if form2.validate_on_submit():
@@ -121,38 +122,53 @@ def register():
 #Login Page 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    """This function logs the user in."""
 
+    #This is the login form grabs the email and password
     form3 = forms.LoginForm()
     email = form3.email.data
     password = form3.password.data
 
     if form3.validate_on_submit():
+        #if user enters wrong username or password they will get error message
+        #they will be redirected to login page again.
+        if User.query.filter_by(email=email).first() is None:
+            flash('Please enter a valid user name and password.', 'login')
+            return redirect('/login')
         login_user= User.query.filter_by(email=email).first()
         session['users_id'] = login_user.id
         
+        #passwords are hashed 
+        #this method is checking the inputed password matches
+        #the stored hashed password
         if(bcrypt.check_password_hash(login_user.password, password)):
-            todays_date = date.today()
-            return redirect(f'/dashboard/{todays_date}')      
+            return redirect('/dashboard')
+        else:
+            flash('Please enter a valid user name and password.', 'login')      
 
     return render_template('login.html', form3=form3)  
 
 #Logout functionality
 @app.route('/logout')
 def logout():
+    """This function logs out the user by clear the session"""
     session.clear()
     return redirect('/')
 
 #Detailed Food Info Page
 @app.route('/view/<food>')
-def foodDetail(food):
+def food_detail(food):
+    """ This function routes the user 
+        to a more detailed view page"""
 
+    #form to log the food to a specific date
     form4 = forms.AddFoodForm()
 
-    result = apiCall(food)
-    print(result)
-
-
-
+    #api call for that food that was selected in the search page
+    result = api_call(food)
+   
+   #formatting the data received from the api call into a dictionary
+   #only grabbing the first item.
     data = {
             'Name': result[0]['food_name'],
             'Serving Unit':f"{result[0]['serving_qty']} {result[0]['serving_unit']}",
@@ -164,20 +180,20 @@ def foodDetail(food):
 
         }
 
-
     return render_template("detailedView.html", data=data, form4=form4)
 
 @app.route('/addedFood/<food>', methods=['GET', 'POST'])
-def addFood(food):
-
+def add_food(food):
+    """This function adds the food to the food log"""
+   
+    #generates the form
     form4 = forms.AddFoodForm()
-    print(food)
     date_logged =form4.date.data
 
-    result = apiCall(food)
-    print(result)
-
-
+    #api call to grab the food chosen to store it in the db
+    result = api_call(food)
+   
+    #formating the data received from the api call
     data = {
     
             'Name': result[0]['food_name'],
@@ -190,10 +206,10 @@ def addFood(food):
 
         }
 
-    addedfood = FoodItem(data['Name'], data['Calorie'], data['Carbohydrate'], data['Protein'], data['Fat'],data['date_logged'], data['user_id'])
-    db.session.add(addedfood)
+    #addingt the food to the data base
+    added_food = FoodItem(data['Name'], data['Calorie'], data['Carbohydrate'], data['Protein'], data['Fat'],data['date_logged'], data['user_id'])
+    db.session.add(added_food)
     db.session.commit()
-
 
     return redirect('/dashboard')
 
@@ -201,6 +217,8 @@ def addFood(food):
 #Deleting food from food log
 @app.route('/delete/<food>', methods=['GET', 'POST'])
 def delete_food(food):
+    """This function deletes food from the food log
+        and removes it from the db"""
     food_to_delete = FoodItem.query.get(food)
     db.session.delete(food_to_delete)
     db.session.commit()
@@ -210,33 +228,33 @@ def delete_food(food):
 # Daily log function returns food log by selected date
 
 def daily_food_log(date_selected):
+    """This function returns the food log by selected date"""
 
-    userFoodList = FoodItem.query.join(User.foodList).filter(User.id==session['users_id'],
+    user_food_list = FoodItem.query.join(User.foodList).filter(User.id==session['users_id'],
     FoodItem.date_logged==date_selected).all()
-    sumCalorie = []
-    sumCarb = []
-    sumPro = []
-    sumFat = []
+    sum_calorie = []
+    sum_carb = []
+    sum_pro = []
+    sum_fat = []
 
-    for fooditem in userFoodList:
-        sumCalorie.append(fooditem.calorie)
-        sumCarb.append(fooditem.carbs)
-        sumPro.append(fooditem.protein)
-        sumFat.append(fooditem.fat)
-
-   
+    for food_item in user_food_list:
+        sum_calorie.append(food_item.calorie)
+        sum_carb.append(food_item.carbs)
+        sum_pro.append(food_item.protein)
+        sum_fat.append(food_item.fat)
 
     # Adding the totals
-    totals = ['Total', round(sum(sumCalorie),2), round(sum(sumCarb),2), round(sum(sumPro),2), round(sum(sumFat),2), '']
+    totals = ['Total', round(sum(sum_calorie),2), round(sum(sum_carb),2), round(sum(sum_pro),2), round(sum(sum_fat),2), '']
   
-    daily_log = [userFoodList, totals]
+    daily_log = [user_food_list, totals]
 
     return daily_log
 
 #creating apicall function for reusability
-def apiCall(food):
+def api_call(food):
+    """This function calls the api"""
     base_url = "https://trackapi.nutritionix.com/v2/search/instant"
-    key = "DEMO_KEY"
+
     headers = {
         "x-app-id": "fc9be373",
         "x-app-key": "46777694be6e90aec315f750629c276b",
@@ -254,8 +272,6 @@ def apiCall(food):
     result = response.json()['common']
 
     return result  
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- classProject/static/script.js
+++ classProject/static/script.js
@@ -2,11 +2,21 @@ const dashCarb = Number(document.getElementById("dash-carb").innerText);
 const dashPro = Number(document.getElementById("dash-pro").innerText);
 const dashFat = Number(document.getElementById("dash-fat").innerText);
 const total = dashCarb + dashPro + dashFat;
+console.log(dashCarb);
+let perCarb;
+let perPro;
+let perFat;
 
 // Calculate the percentage of each macro
-const perCarb = Math.round(100 * (dashCarb / total));
-const perPro = Math.round(100 * (dashPro / total));
-const perFat = Math.round(100 * (dashFat / total));
+if (dashCarb === 0 && dashPro === 0 && dashFat === 0) {
+  perCarb = 100;
+  perPro = 100;
+  perFat = 100;
+} else {
+  perCarb = Math.round(100 * (dashCarb / total));
+  perPro = Math.round(100 * (dashPro / total));
+  perFat = Math.round(100 * (dashFat / total));
+}
 
 // Dashboard Pie Chart
 
